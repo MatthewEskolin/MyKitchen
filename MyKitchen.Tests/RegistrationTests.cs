48c39269a593220212d5b351d9b152026d92ad09
Matthew Eskolin <matteskolin@gmail.com>
2020-05-23 11:30:44 -0500

    add registration test

--- MyKitchen.Tests/RegistrationTests.cs
+++ MyKitchen.Tests/RegistrationTests.cs
@@ -1,21 +1,8 @@
-using System.Linq;
-using Microsoft.AspNetCore.Mvc;
-using Moq;
-using MyKitchen.Controllers;
 using MyKitchen.Data;
-using MyKitchen.Models;
-using MyKitchen.Models.FoodItems;
 using Xunit;
-
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
 
 namespace MyKitchen.Tests
 {
@@ -25,67 +12,21 @@ namespace MyKitchen.Tests
             [Fact]
             public void RegisterNewUser()
             {
-                            //dependencies
-            // var serviceProvider = new ServiceCollection()
-            //     .AddLogging()
-            //     .BuildServiceProvider();
 
             var services = new ServiceCollection();
+
+            services.AddLogging();
+            
             services.AddIdentity<ApplicationUser, Microsoft.AspNetCore.Identity.IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
 
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer( WithTestDatabase.MyConnectionString));
    
 
-            var um = services.BuildServiceProvider().GetService<UserManager<IdentityUser>>();
-            
-            
-            //                     var result = await _userManager.CreateAsync(user, Input.Password);
-
-
-
-
-            #region examples that didn't work
-            //      var _serviceCollection = new ServiceCollection();
-            //          _serviceCollection.AddIdentity<MyKitchen.Data.ApplicationUser, IdentityRole>()
-            //         .AddEntityFrameworkStores<ApplicationDbContext<MyKithen.Data.ApplicationUser>()
-            //     .AddDefaultTokenProviders();
-
-
-
-            // _serviceCollection.AddDbContext<IdentityDbContext<IdentityUser>>(options =>
-            // {
-            //     options.UseSqlServer(connString);
-            // });
-
-
-
-
-            //     var user = new MyKitchen.Data.ApplicationUser { UserName = Input.Email, Email = Input.Email };
-            //     var userManager = new UserManager<MyKitchen.Data.ApplicationUser>(
-
-            //         new UserStoreBase<MyKitchen.Data.ApplicationUser,
-
-
-            //     );
-
-
-    //public UserManager(IUserStore<TUser> store, IOptions<IdentityOptions> optionsAccessor, IPasswordHasher<TUser> passwordHasher, IEnumerable<IUserValidator<TUser>> userValidators, IEnumerable<IPasswordValidator<TUser>> passwordValidators, ILookupNormalizer keyNormalizer, IdentityErrorDescriber errors, IServiceProvider services, ILogger<UserManager<TUser>> logger);
-
-        //
-
-
-
-                // var userManager = new UserManager<MyKitchen.Data.ApplicationUser>(new UserStore<MyKitchen.Data.ApplicationUser,CustomRole,string,CustomUserLogin,CustomuserRole,CustomUserClaim>();
-
-                // new UserStore<MyKitchen.Data.ApplicationUser, CustomRole, string, CustomUserLogin, CustomUserRole, CustomUserClaim>(new ApplicationDbContext()));
-                //var userManager = new userManager
-
-
-
-
-
-#endregion
+            var um = services.BuildServiceProvider().GetService<UserManager<ApplicationUser>>();
+            var newUser = new ApplicationUser(){Email = "test1@test.com"};
+            var result =  um.CreateAsync(newUser, "111111").GetAwaiter().GetResult();
 
 
             }
