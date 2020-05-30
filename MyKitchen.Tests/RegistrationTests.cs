e1f904af8ab85af4b4c7dc29ce3d0b6d60c4d874
Matthew Eskolin <matteskolin@gmail.com>
2020-05-30 16:57:06 -0500

    fix tests

--- MyKitchen.Tests/RegistrationTests.cs
+++ MyKitchen.Tests/RegistrationTests.cs
@@ -3,6 +3,7 @@ using Xunit;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace MyKitchen.Tests
 {
@@ -10,30 +11,60 @@ namespace MyKitchen.Tests
 
 
             [Fact]
-            public void RegisterNewUser()
+            public void RegisterNewUser1()
             {
+                //append guid so test coan be repeated
+                var guid = Guid.NewGuid().ToString();
+                
+                var services = new ServiceCollection();
 
-            var services = new ServiceCollection();
+                services.AddLogging();
+                
+                services.AddIdentity<ApplicationUser, Microsoft.AspNetCore.Identity.IdentityRole>()
+                    .AddEntityFrameworkStores<ApplicationDbContext>()
+                    .AddDefaultTokenProviders();
 
-            services.AddLogging();
-            
-            services.AddIdentity<ApplicationUser, Microsoft.AspNetCore.Identity.IdentityRole>()
-                .AddEntityFrameworkStores<ApplicationDbContext>()
-                .AddDefaultTokenProviders();
+                services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer( WithTestDatabase.MyConnectionString));
+    
 
-            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer( WithTestDatabase.MyConnectionString));
-   
-
-            var um = services.BuildServiceProvider().GetService<UserManager<ApplicationUser>>();
-            var newUser = new ApplicationUser(){Email = "test1@test.com"};
-            var result =  um.CreateAsync(newUser, "111111").GetAwaiter().GetResult();
-
+                var um = services.BuildServiceProvider().GetService<UserManager<ApplicationUser>>();
+                var newUser = new ApplicationUser(){Email = $"{guid}@test.com"};
+                var result =  um.CreateAsync(newUser, "111111").GetAwaiter().GetResult();
 
             }
 
+            [Fact]
+            public void RegisterNewUser2()
+            {
 
+                //append guid so test coan be repeated
+                var guid = Guid.NewGuid().ToString();
+                
+                var services = new ServiceCollection();
 
+                services.AddLogging();
+                
+                services.AddIdentity<ApplicationUser, Microsoft.AspNetCore.Identity.IdentityRole>()
+                    .AddEntityFrameworkStores<ApplicationDbContext>()
+                    .AddDefaultTokenProviders();
 
+                services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer( WithTestDatabase.MyConnectionString));
+    
+
+                var um = services.BuildServiceProvider().GetService<UserManager<ApplicationUser>>();
+                var newUser = new ApplicationUser(){Email = $"{guid}@test.com"};
+                var result =  um.CreateAsync(newUser, "111111").GetAwaiter().GetResult();
+
+            }
+
+            [Fact]
+            public void FoodItemsAreUserIsolated()
+            {
+                Assert.True(true);
+                //if no users in the system, mark as inconclusive
+                // Assert.inconclusive();
+                // https://xunit.net/docs/shared-context
+                //use the above to track the context!
+            }
     }
-
 }
