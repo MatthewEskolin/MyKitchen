11ca4e6fcadd4783e219512a238129f7d3e93348
Matthew Eskolin <matteskolin@gmail.com>
2021-08-14 17:44:52 -0500

    addings test tasks to vs code

--- MyKitchen.Tests/Unit/MealBuilderControllerTests.cs
+++ MyKitchen.Tests/Unit/MealBuilderControllerTests.cs
@@ -66,7 +66,8 @@ namespace MyKitchen.Tests
                                                        {PageSize = testPageSize};
 
             //act
-            var result = (controller.Index(new MealBuilderIndexViewModel(){PageSize = testPageSize}) as ViewResult)?.ViewData.Model as MealBuilderIndexViewModel;
+
+            var result = (controller.Index(new MealBuilderIndexViewModel()) as ViewResult)?.ViewData.Model as MealBuilderIndexViewModel;
 
             Meal[] mealArray = result.Meals.ToArray();
             Assert.True(mealArray.Length == 2);
