097432d475bf9a9b4cef21a1453206855fd1e74a
Matthew Eskolin <matteskolin@gmail.com>
2021-11-21 20:57:04 -0600

    dashboard progress slow

---
+++ MyKitchen/Models/Dashboard/DashboardViewModel.cs
@@ -0,0 +1,16 @@
+﻿using MyKitchen.Data;
+using System.Collections.Generic;
+
+namespace MyKitchen.Models.Dashboard
+{
+    public class DashboardViewModel
+    {
+        public List<Meal> TodaysMeals { get; set; }
+        
+        public List<Meal> TomorrowsMeals { get; set; }
+
+
+
+
+    }
+}
--- MyKitchen/Pages/Dashboard1.cshtml.cs
+++ MyKitchen/Pages/Dashboard1.cshtml.cs
@@ -4,13 +4,57 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using MyKitchen.Controllers;
+using MyKitchen.Data;
 
 namespace MyApp.Namespace
 {
     public class DashboardModel : PageModel
     {
+
+        public DashboardModel(UserInfo usr, IMealRepository mealRepository, ApplicationDbContext ctx)
+        {
+            //what claims principal is already here?
+            var user = this.User.ToString();
+            //this.CurrentUser = usr;
+            this.MealRepo = mealRepository;
+            this.DbContext = ctx;
+
+          //Need User
+
+        }
+
+        public ApplicationDbContext DbContext { get; set; }
+
+        protected IMealRepository MealRepo { get; set; }
+
+        protected UserInfo CujrrentUser { get; set; }
+
+        public List<Meal> TodaysMeals { get; set; }
+
+        public List<Meal> TomorrowsMeals { get; set; }
+
         public void OnGet()
         {
+            //Load Today's Meals and Tomorrows Meals
+
+            //get events -> then get meals
+            //this.DbContext.Events
+
+
+
+
+            //calculate the right metrics
+
+            //the goal of this app is to eat healthy and delicious foods for every meal, and to minimize time spent planning and thinking about food.
+
+            //create this as a customizable dashboard?
+
+            //! Alert - you have 0 meals planned for the next 5 days
+
+            //check to see if meals are planned for the next 5 days, and display an error message if nothign is planned...
+            //else pull the data from "events" and show it in a dashboard...
         }
     }
 }
