032de958531cfb6a546ccd469837b7b72b472734
Matthew Eskolin <matteskolin@gmail.com>
2021-12-31 14:56:36 -0600

    prepare to test function app timer

--- MyKitchen.KeepAlive/MyKitchenKeepAlive.cs
+++ MyKitchen.KeepAlive/MyKitchenKeepAlive.cs
@@ -12,6 +12,7 @@ namespace MyKitchen.KeepAlive
         public void Run([TimerTrigger("*/5 * * * * *")]TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            log.LogInformation( "We still need to add the actual logic. right now this function app is not doing anything. This message should be logged very 5 minutes.");
         }
     }
 }
