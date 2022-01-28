9317e39fd6e7b8ab3cedc3f8ed6cdde01ab141d1
Matthew Eskolin <matteskolin@gmail.com>
2022-01-27 21:29:33 -0600

    function app

---
+++ MyKitchen.KeepAlive/MyFunctionStartup.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+//[assembly: FunctionsStartup(typeof(MyKitchen.KeepAlive.Startup))]
+
+namespace MyKitchen.KeepAlive
+{
+
+        //public class Startup : FunctionsStartup
+        //{
+        //    //public override void Configure(IFunctionsHostBuilder builder)
+        //    //{
+        //    //    base.Configure(builder);
+        //    //    //builder.addtimers
+        //    //    //builder.Services...AddHttpClient();
+
+        //    //    //builder.Services.AddSingleton<IMyService>((s) => {
+        //    //    //    return new MyService();
+        //    //    //});
+
+        //    //    //builder.Services.AddSingleton<ILoggerProvider, MyLoggerProvider>();
+        //    //}
+        //}
+    }
+
--- MyKitchen.KeepAlive/MyKitchenKeepAlive.cs
+++ MyKitchen.KeepAlive/MyKitchenKeepAlive.cs
@@ -1,22 +1,56 @@
 using System;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using MyKitchen.WarmUp;
 
 namespace MyKitchen.KeepAlive
 {
     public class MyKitchenKeepAlive
     {
         [FunctionName("MyKitchenKeepAlive")]
-        //public void Run([TimerTrigger("0 */15 * * * *")]TimerInfo myTimer, ILogger log)
-        public void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log, IConfiguration config)
+        public void Run_MyitchenKeepAlive([TimerTrigger("0 */20 * * * *")]TimerInfo myTimer, ILogger log)
         {
-            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
-            log.LogInformation( "We still need to add the actual logic. right now this function app is not doing anything. This message should be logged very 5 minutes.");
+            log.LogInformation($"MyKitchenKeepAlive Triggered at: {DateTime.Now}");
+            log.LogInformation("This Message should be logged every 20 minutes");
 
             //try to make call to app-warmup task
+            var warmup = new AppWarmUp();
+
+            log.LogTrace("Warmup Instance Created..");
+            log.LogTrace("Starting Warmup..");
+            warmup.RunWarmup();
+
+            var next = myTimer.Schedule.GetNextOccurrence(DateTime.Now);
+
+            log.LogTrace($"Next Trigger at {next}");
+            log.LogTrace($"Warmup Complete.. next occurance is at ");
         }
+
+
+
+        [FunctionName("MyKitchenKeepAlive2")]
+        public void Run_MyKitchenKeepAlive2([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req, ILogger log)
+        {
+            log.LogInformation($"MyKitchenKeepAlive Triggered at: {DateTime.Now}");
+            log.LogInformation("This Message should be logged every 20 minutes");
+
+            //try to make call to app-warmup task
+            var warmup = new AppWarmUp();
+
+            log.LogTrace("Warmup Instance Created..");
+            log.LogTrace("Starting Warmup..");
+            warmup.RunWarmup();
+
+           // var next = myTimer.Schedule.GetNextOccurrence(DateTime.Now);
+
+            //log.LogTrace($"Next Trigger at {next}");
+            log.LogTrace($"Warmup Complete.. next occurance is at ");
+        }
+
     }
 
 }
