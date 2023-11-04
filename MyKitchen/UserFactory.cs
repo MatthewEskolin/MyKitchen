fec84814fae449b55dbe93634e8d77299a97e2a8
matteskolin <matteskolin@gmail.com>
2023-11-04 12:13:26 -0500

    code cleanup

--- MyKitchen/UserFactory.cs
+++ MyKitchen/UserFactory.cs
@@ -1,27 +1,18 @@
-﻿using System;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.Extensions.DependencyInjection;
-using MyKitchen.Data;
-
-namespace MyKitchen;
+﻿namespace MyKitchen;
 
 public class UserFactory
 {
-
     public static ApplicationUser GetUser(IServiceProvider serviceProvider)
     {
-        IHttpContextAccessor accessor = serviceProvider.GetService<IHttpContextAccessor>();
-        UserManager<ApplicationUser> manager = serviceProvider.GetService<UserManager<ApplicationUser>>();
+        var accessor = serviceProvider.GetService<IHttpContextAccessor>();
+        var manager = serviceProvider.GetService<UserManager<ApplicationUser>>();
         if (manager != null)
-        {
             if (accessor != null)
             {
                 var user = manager.GetUserAsync(accessor.HttpContext.User).GetAwaiter().GetResult();
 
                 return user;
             }
-        }
 
         throw new Exception("Failure getting Application User object");
     }
