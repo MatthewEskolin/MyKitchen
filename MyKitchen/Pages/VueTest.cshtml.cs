459a46811c8a99f0fe56e1fb2725567822299026
matteskolin <matteskolin@gmail.com>
2024-03-30 20:00:17 -0500

    minimalistic vue.js example without build steps

---
+++ MyKitchen/Pages/VueTest.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyKitchen.Pages
+{
+    public class VueTestModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
